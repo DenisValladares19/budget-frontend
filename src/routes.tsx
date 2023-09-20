fb14eaf022417d93bc5359345a288522ed6eab2e
Denis Valladares <denisvalladares402@gmail.com>
2023-09-19 21:09:02 -0600

    add private route list

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,12 @@
 import Home from '@pages/Home'
 import SignIn from '@pages/SignIn'
 import SignUp from '@pages/SignUp'
+import { Navigate } from 'react-router-dom'
 
 export const publicRoutes = [
     { path: '/signin', element: <SignIn /> },
     { path: '/signup', element: <SignUp /> },
-    { path: '/', element: <Home /> },
+    { path: '*', element: <Navigate to={'/signin'} /> },
 ]
+
+export const privateRoute = [{ path: '/', element: <Home /> }]
