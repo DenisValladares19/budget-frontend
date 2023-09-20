67b0243fbe1fb10f853fd38b781d2a3d57da29b5
Denis Valladares <denisvalladares402@gmail.com>
2023-09-20 17:36:47 -0600

    fix security

--- src/App.tsx
+++ src/App.tsx
@@ -5,35 +5,26 @@ import { UserDTO } from './interfaces/UserDTO'
 import LoadingFullScreen from '@components/LoadingFullScreen'
 import { useEffect } from 'react'
 import useUser from '@hooks/useUser'
-import { PREFIX_STORAGE } from '@helpers/Constants.utils'
 
 function App() {
     const publicRoute = useRoutes(publicRoutes)
     const mainRoute = useRoutes(privateRoute)
-    const token = localStorage.getItem(PREFIX_STORAGE + 'token')
+    const token = localStorage.getItem('token')
     const [stateUser, fetchUser] = useApi<UserDTO>()
     const { userData, methods } = useUser()
 
     useEffect(() => {
-        console.log(!userData.data && Boolean(token))
-        console.log(!userData.data, Boolean(token))
-
-        if (!userData.data && Boolean(token)) {
+        if (!userData.data && token) {
             fetchUser({
                 method: 'GET',
                 url: '/users/info',
             }).then((res) => {
                 methods.onUpdate(res.data)
-                console.log(res)
             })
         }
-    }, [token])
+    }, [])
 
     if (stateUser.isLoading) {
-        return <LoadingFullScreen />
-    }
-
-    if (!token && userData.data) {
         return <LoadingFullScreen />
     }
 
